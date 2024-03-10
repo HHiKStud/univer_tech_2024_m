44e9c95abd45463ec67e86c54f666858a2faa2cd
HHiKStud <1hellstt2@gmail.com>
2024-03-10 14:44:42 +0000

    third

--- my_react_app/src/components/Header/Header.jsx
+++ my_react_app/src/components/Header/Header.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Header = () => {
     return (
-        <div style = {{ border: '1px solid #333', height: '40px'}}>Header</div>
+        <div style = {{ border: '1px solid #333', height: '40px', position: 'fixed', width: '100%', background: '#fff'}}>Header</div>
     )
 }
--- my_react_app/src/components/Layout/Layout.jsx
+++ my_react_app/src/components/Layout/Layout.jsx
@@ -1,17 +1,24 @@
-import React from "react"
+import React, { useRef } from "react"
 import { Header } from "../Header/Header"
 import { Footer } from "../Footer/Footer"
 
+import './Layout.css'
+
 export const Layout = ({children}) => {
+    const textareaRef = useRef()
+    
     return (
-        <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            height: '768px',
-            justifyContent: 'space-between'
-          }}>
+        <div className="layout_container">
             <Header />
-            {children}
+            
+            <div className="child_container" style={{
+                margin: '0 auto',
+                width: '64 em'
+            }}>
+                <textarea ref={textareaRef}></textarea>
+                <input type='button' value={'Get text'} onClick={() => {console.log('ref', textareaRef.current.value)}}/>   
+                {children}
+            </div>
             <Footer />
         </div>
     )
