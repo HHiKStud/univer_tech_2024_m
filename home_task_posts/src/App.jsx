423e48e1cc21b991129ab6dd8667addd6f6150ce
HHiKStud <1hellstt2@gmail.com>
2024-03-13 18:15:32 +0000

    posts onClick done w/ simple Design

--- home_task_posts/src/App.jsx
+++ home_task_posts/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 
 import { PageLayout } from './components/Layout/Layout';
 import { useEffect, useState } from 'react';
-import { Routes, Route, useParams, useLocation, Navigate } from 'react-router-dom';
+import { Routes, Route, useParams, useLocation, Navigate, Link } from 'react-router-dom';
 
 function App() {
   const [ data, setData ] = useState([]);
@@ -25,6 +25,7 @@ function App() {
    <Routes>
     <Route path='/*' element={<PageLayout/>}>
       <Route index element={<HomeComponent data={data}/>} />
+      <Route path='post/:postId' element={<LinkPost/>}/>
     </Route>
 
     <Route path='/auth/'>
@@ -39,16 +40,42 @@ function App() {
 
 export default App;
 
-const HomeComponent = ({data}) => {
+const HomeComponent = ({data}, props) => {
+  {console.log(data)}
   return (
     <div style={{display: 'flex', flexDirection: 'column', gap: '8px', width: '64em', margin: '0 auto', marginTop: '15px'}}>
       {data.map(({ post, user }) => (
         <div style = {{border: '1px solid #333', padding: '12px', borderRadius: '8px'}}>
-          <h2>{post.title}</h2>
-          <h3>{user.name}</h3>
-          <p>{post.body}</p>
+            <Link to={`/post/${post.id}`}
+              state={{
+                id: post.id, 
+                user: user.name, 
+                postB: post.body, 
+                postT: post.title
+              }}>
+            <h2>{post.title}</h2>
+            <h3>{user.name}</h3>
+            <p>{post.body}</p>
+          </Link>
         </div>
       ))}
     </div> 
+  )
+}
+
+const LinkPost = () => {
+  const location = useLocation()
+
+  return (
+    <div>
+      <div style = {{border: '1px solid #333', padding: '12px', borderRadius: '8px'}}>
+        <h3>Author: {location.state.user}</h3>
+        <h4>Title: {location.state.postT}</h4>
+        <p>{location.state.postB}</p>
+      </div>
+      <div style = {{border: '1px solid #333', padding: '12px', borderRadius: '8px'}}>
+        <h2>Comments:</h2>
+      </div>
+    </div>
   )
 }
